24fd0fe7dbc3269a98983f4a52473a2f4c1c8840
mohammedjunaid09 <mkjunaiduddin815@gmail.com>
2024-05-25 10:38:53 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,7 @@
+let mulof3=[]
+
+for(i=1; i<=100; i++){
+if(i%3==0)
+    mulof3.push(i)
+}
+ console.log(mulof3)
---
+++ 2.js
@@ -0,0 +1,7 @@
+function countnum(a){
+
+for(i=0; i<=100; i++){
+    console.log(i)
+}
+}
+countnum(100)
